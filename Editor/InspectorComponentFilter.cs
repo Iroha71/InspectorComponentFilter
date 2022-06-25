7e50c0377585b759fefef4f7c6fe3a339be1a6a0
Iroha71 <y.koba.h1071@outlook.jp>
2022-06-25 15:29:28 +0900

    [update] EditorWindow上でコンポーネントを閲覧できるように変更

--- Editor/InspectorComponentFilter.cs
+++ Editor/InspectorComponentFilter.cs
@@ -1,83 +1,116 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.Linq;
 
 namespace InspectorComponentFilter {
+    /// <summary>
+    /// オブジェクトの特定のコンポーネントをウィンドウに表示する機能を実装する
+    /// </summary>
     public class InspectorComponentFilter : EditorWindow {
-        private string filter;
-        private GameObject target;
-        private Dictionary<string, Component> components = new Dictionary<string, Component>();
-        private int instanceId;
+        /// <summary>
+        /// 検索するコンポーネント名
+        /// </summary>
+        private string searchComponentName;
+        private GameObject obj;
+        private Dictionary<string, Editor> drawEditors = new Dictionary<string, Editor>();
+        /// <summary>
+        /// オブジェクトにアタッチされているすべてのコンポーネント
+        /// </summary>
+        private Dictionary<string, Component> attachedComponents = new Dictionary<string, Component>();
+        /// <summary>
+        /// コンポーネント一覧の現在のスクロール位置
+        /// </summary>
+        private Vector2 componentViewScrollPosition = Vector2.zero;
         [MenuItem("Tools/InspectorComponentFilter")]
-        private static void Init() {
-            GetWindow<InspectorComponentFilter>("Search Component");
+        private static void Open() {
+            GetWindow<InspectorComponentFilter>();
         }
 
-        private void OnGUI() {
-            if (instanceId != Selection.activeInstanceID) {
-                ChangeHideFlags(components, HideFlags.None);
-                instanceId = Selection.activeInstanceID;
-                target = Selection.activeGameObject;
-            }
-            GetAttachedCompoentns(target);
+        /// <summary>
+        /// コンポーネント検索欄を描画する
+        /// </summary>
+        private void DrawSearchBox() {
             using (new EditorGUILayout.HorizontalScope()) {
                 GUILayout.Label("Filter:", GUILayout.Width(45));
                 GUI.SetNextControlName("filterField");
-                EditorGUI.BeginChangeCheck();
-                filter = GUILayout.TextField(filter, "SearchTextField", GUILayout.Width(120));
-                if (EditorGUI.EndChangeCheck() && !string.IsNullOrEmpty(filter)) {
-                    Dictionary<string, Component> matchedComponents = components.Where(com => com.Key.StartsWith(filter.ToLower())).ToDictionary(x => x.Key, x => x.Value);
-                    if (matchedComponents.Count > 0) {
-                        foreach (string key in components.Keys) {
-                            if (matchedComponents.ContainsKey(key))
-                                SwitchComponent(components[key], HideFlags.None);
-                            else
-                                SwitchComponent(components[key], HideFlags.HideInInspector);
-                        }
-                    }
-                } else if (string.IsNullOrEmpty(filter)) {
-                    foreach (string key in components.Keys) {
-                        ChangeHideFlags(components, HideFlags.None);
-                    }
-                }
-                GUI.FocusControl("filterField");
-                GUI.enabled = !string.IsNullOrEmpty(filter);
+                searchComponentName = GUILayout.TextField(searchComponentName, "SearchTextField", GUILayout.Width(120));
                 if (GUILayout.Button("Clear", "SearchCancelButton")) {
-                    filter = string.Empty;
-                    ChangeHideFlags(components, HideFlags.HideInInspector);
+                    searchComponentName = string.Empty;
                 }
-                GUI.enabled = true;
             }
         }
 
-        private void ChangeHideFlags(Dictionary<string, Component> components, HideFlags hideFlags, Dictionary<string, Component> matchedComponents=null) {
-            foreach (string key in components.Keys) {
-                SwitchComponent(components[key], hideFlags);
+        private void OnGUI() {
+            EditorGUI.BeginChangeCheck();
+            obj = EditorGUILayout.ObjectField("Obj", obj, typeof(GameObject), true) as GameObject;
+            DrawSearchBox();
+            
+            if (EditorGUI.EndChangeCheck() && obj != null && searchComponentName != string.Empty) {
+                attachedComponents = GetAttachedComponents(obj);
+                drawEditors = FindTargetComponent(searchComponentName);
+            } 
+            DrawMatchedComponentList(drawEditors);
+        }
+
+        /// <summary>
+        /// オブジェクトに取り付けられている全てのコンポーネントを取得する
+        /// </summary>
+        /// <param name="obj">コンポーネントを取得するオブジェクト</param>
+        /// <returns>コンポーネント名を小文字に変換した辞書</returns>
+        private Dictionary<string, Component> GetAttachedComponents(GameObject obj) {
+            Component[] components = obj.GetComponents<Component>();
+            Dictionary<string, Component> attachedComponents = new Dictionary<string, Component>();
+            foreach (Component attachedComponent in components) {
+                attachedComponents.Add(attachedComponent.GetType().Name.ToLower(), attachedComponent);
             }
+
+            return attachedComponents;
         }
 
-        private void SwitchComponent(Component component, HideFlags flags) {
-            if (!component)
+        /// <summary>
+        /// 検索条件に合致したコンポーネントを一覧表示する
+        /// </summary>
+        /// <param name="matchedComponentEditors">検索条件に一致したコンポーネントのEditor情報</param>
+        private void DrawMatchedComponentList(Dictionary<string, Editor> matchedComponentEditors) {
+            if (matchedComponentEditors == null)
                 return;
-            if (component.hideFlags == flags)
-                return;
-            component.hideFlags = flags;
-        }
-
-        private void GetAttachedCompoentns(GameObject target) {
-            if (!target)
-                return;
-            Component[] components = target.GetComponents<Component>();
-            this.components.Clear();
-            foreach (Component com in components) {
-                this.components.Add(com.GetType().Name.ToLower(), com);
+            
+            componentViewScrollPosition = EditorGUILayout.BeginScrollView(componentViewScrollPosition);
+            foreach (string componentName in matchedComponentEditors.Keys) {
+                GUILayout.Space(10);
+                GUILayout.Box("", GUILayout.ExpandWidth(true), GUILayout.Height(1));
+                EditorGUILayout.LabelField(componentName);
+                matchedComponentEditors[componentName].OnInspectorGUI();
             }
+            EditorGUILayout.EndScrollView();
         }
 
-        private void OnDestroy() {
-            ChangeHideFlags(components, HideFlags.None);
+        /// <summary>
+        /// 特定の名称のコンポーネントを検索し、Editor情報を返す
+        /// </summary>
+        /// <param name="serachComponentName">検索したいコンポーネント名</param>
+        /// <returns>Inspectorに描画するEditor情報</returns>
+        private Dictionary<string, Editor> FindTargetComponent(string searchComponentName) {
+            Dictionary<string, Component> matchedComponents;
+            if (!string.IsNullOrEmpty(searchComponentName)) {
+                 matchedComponents = attachedComponents
+                .Where(attachedComponent => attachedComponent.Key.StartsWith(searchComponentName.ToLower()))
+                .ToDictionary(x => x.Key, x => x.Value);
+            } else {
+                matchedComponents = attachedComponents;   
+            }
+
+            if (matchedComponents.Count <= 0)
+                return null;
+
+            Dictionary<string, Editor> matchedComponentEditors = new Dictionary<string, Editor>();
+            foreach (string key in matchedComponents.Keys) {
+                matchedComponentEditors.Add(matchedComponents[key].GetType().Name, Editor.CreateEditor(matchedComponents[key]));
+            }
+
+            return matchedComponentEditors;
         }
     }
 }
